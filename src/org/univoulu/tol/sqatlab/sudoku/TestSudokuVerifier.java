05251351de6b60691c9a3bf61f54bca91a4271d0
somename <someemail>
2015-10-02 12:42:08 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -19,4 +19,8 @@ public class TestSudokuVerifier {
 		assertEquals(0, game.possibleSolution());
 	}
 
+	@Test
+	public void testInputStringIsValid(){
+		assertEquals(0, game.possibleSolution());
+	}
 }
