e9707627c76cac13ffbb538f088e1fcb98ce9303
somename <someemail>
2015-10-02 12:31:28 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -5,15 +5,25 @@ public class SudokuVerifier {
 	String valid = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 	String invalid = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	
-	public void possibleSolution(){
+	public int possibleSolution(){
 		switch(verify(valid)){
-		case 0: System.out.println("Valid Solution"); break;
+		case 0: System.out.println("Valid Solution"); return 0;
+		case -1: System.out.println("Invalid Input"); return -1;
+		case -2: System.out.println("Number repeated in subgrid"); return -2;
+		case -3: System.out.println("Number repeated in row"); return -3;
+		case -4: System.out.println("Number repeated in column"); return -4;
+		case -5: System.out.println("Invalid Input"); return -5;
+		default: return 1;
 		}
 		
 	}
 	
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
+		//check length
+		if(candidateSolution.length()!=81) return -5;
+		
+		//subgrid validity
+		
 		return 0;
 	}
 }
