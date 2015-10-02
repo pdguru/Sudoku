02a86d061fe7d23ebfdd05481637e547b5100947
somename <someemail>
2015-10-02 12:11:46 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -2,6 +2,16 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuVerifier {
 
+	String valid = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	String invalid = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+	
+	public void possibleSolution(){
+		switch(verify(valid)){
+		case 0: System.out.println("Valid Solution"); break;
+		}
+		
+	}
+	
 	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
 		return 0;
